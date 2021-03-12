af23f70f004674314c0a27931db5cef149fcb2bc
udayaditya singh <udayaditya.singh@gmail.com>
2021-03-12 10:54:13 +0530

    "changes"

--- src/Components/validatorRules/ForgotPasswordValidator.js
+++ src/Components/validatorRules/ForgotPasswordValidator.js
@@ -1,23 +1,23 @@
 
-// const ForgotPasswordValidator = {
-//     email: {
-//       rules: [
-//         {
-//           test: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
-//           message: "Email must be in the form of abc@xyz.com",
-//         },
-//         {
-//           test: (value) => {
-//             return Object.keys(value).length !== 0;
-//           },
-//           message: "Email cannot be empty",
-//         },
-//       ],
-//       errors: [],
-//       valid: false,
-//       state: "",
-//     },
+const ForgotPasswordValidator = {
+    email: {
+      rules: [
+        {
+          test: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+          message: "Email must be in the form of abc@xyz.com",
+        },
+        {
+          test: (value) => {
+            return Object.keys(value).length !== 0;
+          },
+          message: "Email cannot be empty",
+        },
+      ],
+      errors: [],
+      valid: false,
+      state: "",
+    },
 
-//   };
-//   export default ForgotPasswordValidator;
+  };
+  export default ForgotPasswordValidator;
   
