9dfc6560804cc51220bc48179046300a04a9b40d
udayaditya singh <udayaditya.singh@gmail.com>
2021-07-09 18:32:09 +0530

    "changes"

--- src/Components/config/default.js
+++ src/Components/config/default.js
@@ -1,5 +1,5 @@
-const uRL = "http://3.101.47.250";
-// const uRL = "https://api.finmagix.com";
+// const uRL = "http://3.101.47.250";
+const uRL = "https://api.finmagix.com";
 
 // const uRL = "http://localhost:8000";
 // const UserID = process.env.NODE_ENV === "production" ? "5e578febcf30c039b6173789" : "5e5ca890e5097420dde96850";
